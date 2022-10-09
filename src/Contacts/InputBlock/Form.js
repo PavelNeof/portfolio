888e9d3be9912dbcda4fb223c5805e23c0da6a62
PavelNeof <pavelneof@gmail.com>
2022-10-09 15:17:21 +0300

    portfolio

--- src/Contacts/InputBlock/Form.js
+++ src/Contacts/InputBlock/Form.js
@@ -5,9 +5,9 @@ import s from './Form.module.css'
 function Form() {
     return (
         <div className={s.inputBlock}>
-            <input placeholder={'input'}/>
-            <input placeholder={'input'}/>
-            <textarea className={s.textarea} placeholder={'textarea'}/>
+            <input placeholder={'input'} className={s.input}/>
+            <input placeholder={'input'} className={s.input}/>
+            <textarea className={s.textarea} placeholder={'textarea'} className={s.textarea}/>
         </div>
     );
 }
