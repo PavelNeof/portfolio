f8d504c8c9f05df816c02fee5549b8320682de2e
PavelNeof <pavelneof@gmail.com>
2022-11-13 16:07:15 +0300

    portfolio

--- src/Skills/Skill/Skill.js
+++ src/Skills/Skill/Skill.js
@@ -4,14 +4,24 @@ import s from './Skill.module.css'
 
 function Skill(props) {
     return (
-        <div className={s.skill}>
-            <div className={s.icon}></div>
-            <h3 className={s.title}>{props.title}</h3>
-            <span className={s.description}>
-                {props.description}
-            </span>
+         <div className={s.skill}>
+             <div className={s.icon}></div>
+             <h3 className={s.title}>{props.title}</h3>
+             <p className={s.description}>
+                 {props.description}
+             </p>
 
-        </div>
+            {/*<div className="hover-text-one">*/}
+            {/*    <figure className="effect-text-two">*/}
+            {/*        <img src="https://klike.net/uploads/posts/2018-06/1528641301_5.jpg" alt=""/>*/}
+            {/*        <figcaption>*/}
+            {/*            <h3>Lorem <span>ipsum</span></h3>*/}
+            {/*            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>*/}
+            {/*        </figcaption>*/}
+            {/*    </figure>*/}
+            {/*</div>*/}
+
+         </div>
     );
 }
 
--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -11,14 +11,13 @@ function Skills() {
                 <div className={s.skills}>
                     <Skill title={'JavaScript'} description={'functional components, class components'}/>
                     <Skill title={'TypeScript'} description={'functional components, class components'} />
-                    <Skill title={'React'} description={'Redux, Route, Redux-Thunk, Formik'}/>
-                    <Skill title={'Redux'} description={''}/>
+                    <Skill title={'React'} description={'Redux, Route, Formik'}/>
+                    <Skill title={'Redux'} description={'Redux-Thunk, reducers'}/>
                     <Skill title={'Git'} description={'GitHab, work in a team, git merge'}/>
-                    <Skill title={'CSS'} description={'adaptive layout, SASS, BEM, Bootstrap, Material UI'}/>
-                    <Skill title={'Storybook'} description={''}/>
-                    <Skill title={'Rest Api'} description={'Axios, Fetch, Postman'}/>
-                    <Skill title={'Formik'} description={''}/>
-                    <Skill title={'UNIT-TESTING'} description={''}/>
+                    <Skill title={'CSS'} description={'adaptive layout, SASS, BEM, Tailwind CSS, Material UI'}/>
+                    <Skill title={'Storybook'} description={'Create component'}/>
+                    <Skill title={'Rest Api'} description={'Axios, Postman'}/>
+                    <Skill title={'Unit testing'} description={'Unit testing component'}/>
                     {/*<Skill title={'React'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
                         'Nam rhoncus fringilla lacus in venenatis. Nam tristique lectus sed fringilla scelerisque. ' +
                         'Pellentesque iaculis accumsan felis. Mauris suscipit dui quis sem venenatis, quis ultricies tellus sagittis. ' +
