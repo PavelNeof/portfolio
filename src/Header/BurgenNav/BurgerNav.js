911a53227878ff8932b6d38b3d73538abe13bbb3
PavelNeof <pavelneof@gmail.com>
2022-11-16 18:23:42 +0300

    portfolio

--- src/Header/BurgenNav/BurgerNav.js
+++ src/Header/BurgenNav/BurgerNav.js
@@ -4,12 +4,13 @@ import {Link, animateScroll as scroll} from "react-scroll";
 import menu from "./../../assets/image/menu.png"
 
 function BurgerNav() {
+
     const [isOpen, setIsOpen] = useState(false)
 
-     const onBurgerButtonClick = () => {
-         setIsOpen(!isOpen)
-         console.log(isOpen)
-     }
+    const onBurgerButtonClick = () => {
+        setIsOpen(!isOpen)
+        console.log(isOpen)
+    }
 
     const Menu = {
         backgroundImage: `url(${menu})`,
@@ -17,8 +18,8 @@ function BurgerNav() {
 
     return (
         <nav className={s.burgerNav}>
-             <div className={ isOpen ? s.burgerNavItems : `${s.burgerNavItems} ${s.close}` } >
-                  <Link className={s.linkUnderlineMenu} activeClass={s.active} to={'main'} spy={true} smooth={true}
+            <div className={isOpen ? s.burgerNavItems : `${s.burgerNavItems} ${s.close}`}>
+                <Link className={s.linkUnderlineMenu} activeClass={s.active} to={'main'} spy={true} smooth={true}
                       offset={-70} duration={500}>Main</Link>
                 <Link className={s.linkUnderlineMenu} activeClass={s.active} to={'skills'} spy={true} smooth={true}
                       offset={-70} duration={500}>Skills</Link>
@@ -27,8 +28,8 @@ function BurgerNav() {
                 <Link className={s.linkUnderlineMenu} activeClass={s.active} to={'contacts'} spy={true} smooth={true}
                       offset={-70} duration={500}>Contact</Link>
             </div>
-            <div  className={s.burgerNavButton} onClick={onBurgerButtonClick}>
-
+            <div className={s.burgerNavButton} onClick={onBurgerButtonClick}>
+                <svg className={s.img} style={Menu}></svg>
             </div>
         </nav>
     );
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,11 +3,13 @@ import s from './Header.module.css';
 import Nav from "./Nav/Nav";
 import BurgerNav from "./BurgenNav/BurgerNav";
 
+
 function Header() {
+
     return (
         <header className={s.header}>
             <Nav/>
-            <BurgerNav/>
+            <BurgerNav />
         </header>
     );
 }
