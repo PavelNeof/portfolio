faa266b31d797806deca1b9e2c00a78727c22e92
PavelNeof <pavelneof@gmail.com>
2022-11-27 13:34:07 +0300

    portfolio works description

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import s from "./Main.module.css";
 import sContainer from "./../Common/Styles/Container.module.css"
 import Zoom from 'react-reveal/Zoom';
+import cv from "../Common/CV/CV.txt";
 
 function Main() {
     return (
@@ -12,6 +13,7 @@ function Main() {
                     <span>Hi, bro</span>
                     <h1>I'm Pavel Neofitov</h1>
                     <p>Frontend Developer</p>
+                    <a href={cv} download="CV download">CV download</a>
                 </div>
                 <div className={s.photo}></div>
                 </Zoom>
