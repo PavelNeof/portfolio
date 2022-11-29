a022f4194776dcb20fd68f0063480e5857cfa2ac
PavelNeof <pavelneof@gmail.com>
2022-11-29 19:11:12 +0300

    portfolio fix min-height block main, contact, footer

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,17 +1,10 @@
 import {React, useRef} from "react";
 import s from './Contacts.module.css'
 import Form from "./InputBlock/Form";
-import Button from "../Common/Button/Button";
-
-import Zoom from 'react-reveal/Zoom';
 
 function Contacts() {
 
-    const form=useRef()
 
-    const sendEmail =() =>{
-
-    }
     return (
         <div className={s.contacts} id={'contacts'}>
 
